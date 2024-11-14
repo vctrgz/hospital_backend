8818fbf5c4b5d38877b6635dc87cc6b4a1b60399
VictorGonzalez <goncavic2002@gmail.com>
2024-11-14 15:51:31 +0100

    Create new nurse

--- src/main/java/springBoot/hospital/dao/NurseRepository.java
+++ src/main/java/springBoot/hospital/dao/NurseRepository.java
@@ -3,7 +3,10 @@ package springBoot.hospital.dao;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import springBoot.entity.Nurse;
 
@@ -14,4 +17,21 @@ import springBoot.entity.Nurse;
 public interface NurseRepository extends CrudRepository<Nurse, Integer> {
 	Optional<Nurse> findByUserAndPassword(String user, String password);
 	Optional<Nurse> findByName(String name);
+	  // Método para actualizar el nombre del enfermero por su ID
+    @Modifying
+    @Transactional
+    @Query("UPDATE Nurse n SET n.name = :name WHERE n.id = :id")
+    void updateNameById(String name, Integer id);
+
+    // Método para actualizar el usuario del enfermero por su ID
+    @Modifying
+    @Transactional
+    @Query("UPDATE Nurse n SET n.user = :user WHERE n.id = :id")
+    void updateUserById(String user, Integer id);
+
+    // Método para actualizar la contraseña del enfermero por su ID
+    @Modifying
+    @Transactional
+    @Query("UPDATE Nurse n SET n.password = :password WHERE n.id = :id")
+    void updatePasswordById(String password, Integer id);
 }
